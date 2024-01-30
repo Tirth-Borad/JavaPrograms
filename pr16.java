041c5740db9dcfdbdaadbe15fc905b61acb8cd43
Tirth Borad <tborad102@gmail.com>
2024-01-30 10:02:43 +0530

    Uploaded more programs.

---
+++ pr16.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class adder {
+    int add(int a, int b) {
+        return (a + b);
+    }
+}
+
+public class pr16 {
+    public static void main(String[] args) {
+        int n1, n2;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter Number 1 :-");
+            n1 = sc.nextInt();
+            System.out.println("Enter Number 2 :-");
+            n2 = sc.nextInt();
+        }
+        adder a = new adder();
+        System.out.println(a.add(n1, n2));
+    }
+}
